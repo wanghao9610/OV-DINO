ef272b6d793273844ea7fb16e2633e699a3f3cdd
wanghao9610 <wanghao9610@outlook.com>
2024-08-15 18:21:06 +0800

    Update custom_ovd.py

--- ovdino/configs/common/data/custom_ovd.py
+++ ovdino/configs/common/data/custom_ovd.py
@@ -15,10 +15,10 @@ from omegaconf import OmegaConf
 dataloader = OmegaConf.create()
 
 # If you want to define it by yourself, you can change it on ovdino/detrex/data/datasets/custom_ovd.py, you need to uncomment the code (ovdino/detrex/data/datasets/__init__.py L21) first.
-# If you follow the coco format, you need uncomment and change the following code.
+# If you follow the coco format, you need uncomment and change the following code (Recommend).
 # register_custom_ovd_instances(
 #     "custom_train_ovd_unipro",  # dataset_name
-#     {},  # custom_data_info, example: {"thing_dataset_id_to_contiguous_id": {0: 0, 1: 0}, "thing_classes": ["category_0", "category_1"]}
+#     {"thing_dataset_id_to_contiguous_id": {0: 0, 1: 0}, "thing_classes": ["category_0", "category_1"]},  # custom_data_info, just a example.
 #     "/path/to/train.json",  # annotations_json_file
 #     "/path/to/train/images",  # image_root
 #     2,  # number_of_classes, default: 2
@@ -26,7 +26,7 @@ dataloader = OmegaConf.create()
 # )
 # register_custom_ovd_instances(
 #     "custom_val_ovd_unipro",
-#     {}, # custom_data_info, example: {"thing_dataset_id_to_contiguous_id": {0: 0, 1: 0}, "thing_classes": ["category_0", "category_1"]}
+#     {"thing_dataset_id_to_contiguous_id": {0: 0, 1: 0}, "thing_classes": ["category_0", "category_1"]},  # custom_data_info, just a example.
 #     "/path/to/val.json",
 #     "/path/to/val/images",
 #     2,
@@ -34,7 +34,7 @@ dataloader = OmegaConf.create()
 # )
 # register_custom_ovd_instances(
 #     "custom_test_ovd",
-#     {}, # custom_data_info, example: {"thing_dataset_id_to_contiguous_id": {0: 0, 1: 0}, "thing_classes": ["category_0", "category_1"]}
+#     {"thing_dataset_id_to_contiguous_id": {0: 0, 1: 0}, "thing_classes": ["category_0", "category_1"]},  # custom_data_info, just a example.
 #     "/path/to/test.json",
 #     "/path/to/test/images",
 #     2,
